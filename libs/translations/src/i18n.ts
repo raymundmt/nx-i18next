072143a81dbb333c13479439b79f86c9ff0b04ff
Raymund Tienzo <mon2x0@gmail.com>
2024-03-06 23:58:08 +0800

    configure i18next-resources-to-backend and move translations into react library

--- libs/translations/src/i18n.ts
+++ libs/translations/src/i18n.ts
@@ -1,17 +1,18 @@
 import i18n from 'i18next';
+import resourcesToBackend from 'i18next-resources-to-backend';
 import { initReactI18next } from 'react-i18next';
 
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 // don't want to use this?
 // have a look at the Quick start guide 
 // for passing in lng and translations on init
 
 i18n
+    .use(resourcesToBackend((language: string, namespace: string) => import(`./assets/locales/${language}/${namespace}.json`)))
+    
     // load translation using http -> see /public/locales (i.e. https://github.com/i18next/react-i18next/tree/master/example/react/public/locales)
     // learn more: https://github.com/i18next/i18next-http-backend
     // want your translations to be loaded from a professional CDN? => https://github.com/locize/react-tutorial#step-2---use-the-locize-cdn
-    .use(Backend)
     // detect user language
     // learn more: https://github.com/i18next/i18next-browser-languageDetector
     .use(LanguageDetector)
@@ -26,11 +27,6 @@ i18n
 
         interpolation: {
             escapeValue: false, // not needed for react as it escapes by default
-        },
-        // define the backend options
-        backend: {
-            // for all available options read the backend's repository readme file
-            loadPath: '/locales/{{lng}}/{{ns}}.json',
         },
     });
 
